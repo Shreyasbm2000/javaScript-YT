c25c34560baf234c22c74fe210afddf75c74f750
Shreyasbm2000 <105931383+Shreyasbm2000@users.noreply.github.com>
2023-12-20 12:43:05 +0000

    commited

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId  =1234
+let accountName = "Shreyas"
+var accountPassword = "7565e4w3454"
+accountCity = "Banglore"
+ let accountState;
+
+ console.log(accountId)
+ console.table([accountId,accountName,accountPassword,accountState,accountCity])
